cc905e04b60ef418789f05fb22106144d4640da5
zhao-rgb <1019916061@qq.com>
2020-10-28 11:24:36 +0800

    直播项目初始化

---
+++ App.vue
@@ -0,0 +1,21 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+			//监听底部导航中间凸起按钮
+			uni.onTabBarMidButtonTap(() =>{
+				console.log('点击了中间按钮')
+			})
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
