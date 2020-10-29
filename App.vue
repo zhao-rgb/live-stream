bc75fbf58ea118797cf7c33cd625519e452a623d
zhao-rgb <1019916061@qq.com>
2020-10-29 18:39:10 +0800

    弹幕组件

--- App.vue
+++ App.vue
@@ -2,6 +2,12 @@
 export default {
 	onLaunch: function() {
 		console.log('App Launch');
+		const domModule = weex.requireModule('dom');
+		domModule.addRule('fontFace', {
+			fontFamily: 'iconfont',
+			src: "url('http://at.alicdn.com/t/font_1859985_7mxozsfdvib.ttf')"
+		});
+
 		//监听底部导航中间凸起按钮
 		uni.onTabBarMidButtonTap(() => {
 			console.log('点击了中间按钮');
---
+++ components/live/f-danmu.vue
@@ -0,0 +1,58 @@
+<template>
+	<view style="position: fixed; bottom: 120rpx; left: 0; right: 0;">
+		<!-- 使用动画纵向滚动，将scrollInToView方法的返回值绑定到scroll-into-view属性上 -->
+		<scroll-view
+			scroll-y="true"
+			style="width: 520rpx; height: 300rpx;"
+			scroll-with-animation
+			class="pl-3"
+			:scroll-into-view="scrollInToView"
+		>
+			<view
+				:id="'danmu' + item.id"
+				class="flex justify-start align-center rounded p-2 mb-2"
+				style="background-color: rgba(255,255,255,0.125);"
+				v-for="(item, index) in list"
+				:key="index"
+			>
+				<text class="font-md text-danger">{{ item.name }}</text>
+				<text class="font-md text-white">{{ item.content }}</text>
+			</view>
+		</scroll-view>
+	</view>
+</template>
+
+<script>
+export default{
+  data(){
+    return{
+      scrollInToView: '', //滚动到哪个元素的view视图
+      list: [] //弹幕数组
+    }
+  },
+  mounted(){
+    let id = 1
+    setInterval(() =>{
+      this.list.push({
+        id:id,
+        name: '观众'+id,
+        content: '发言_'+id
+      })
+      this.toBottom()
+      id++
+    },2000)
+  },
+  methods:{
+    toBottom(){
+      setTimeout(() =>{
+        let len = this.list.length
+        if(len>0&&this.list[len - 1]){
+          this.scrollInToView = 'danmu' + this.list[len - 1].id
+        }
+      },200)
+    }
+  }
+}
+</script>
+
+<style></style>
