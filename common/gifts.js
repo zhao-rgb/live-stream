7ce25b6d9072351ccaf186bd7347045bda806f33
zhao-rgb <1019916061@qq.com>
2020-10-30 15:53:51 +0800

    发送礼物弹出层

---
+++ common/gifts.js
@@ -0,0 +1,50 @@
+export default [
+  {
+    image: '/static/gift/7.png',
+    id: 1,
+    name: '爱心',
+    coin: 1,
+  },
+  {
+    image: '/static/gift/3.png',
+    id: 2,
+    name: '蛋糕',
+    coin: 2,
+  },
+  {
+    image: '/static/gift/8.png',
+    id: 3,
+    name: '巧克力',
+    coin: 3,
+  },
+  {
+    image: '/static/gift/2.png',
+    id: 4,
+    name: '666',
+    coin: 4,
+  },
+  {
+    image: '/static/gift/6.png',
+    id: 5,
+    name: '胶囊',
+    coin: 5,
+  },
+  {
+    image: '/static/gift/1.png',
+    id: 6,
+    name: '煎蛋',
+    coin: 6,
+  },
+  {
+    image: '/static/gift/4.png',
+    id: 7,
+    name: '龙虾',
+    coin: 7,
+  },
+  {
+    image: '/static/gift/5.png',
+    id: 8,
+    name: '溜溜球',
+    coin: 8,
+  },
+]
